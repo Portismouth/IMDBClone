ab6779a332667a0fa77e00ea5fec6815cf497af3
Anne <anne.jurack@gmail.com>
2018-02-26 15:12:28 -0600

    Added ng-bootstrap and basic test of a dropdown menu

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [NgbModule],
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
