ef607bf442acfaed2f56da83014cfcbf6837491d
Portismouth <ozzy.gonzalez@gmail.com>
2018-02-27 11:22:37 -0600

    pushing api call changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,8 +10,12 @@ export class AppComponent implements OnInit {
 
   constructor(private _httpService: HttpService) { }
 
+  movieList = [];
+
   ngOnInit() {
     this.searchResultsFromService("fight club");
+    this.currentWeekReleaseFromService();
+    this.alreadyOutFromService();
   }
 
   searchResultsFromService(query, type?) {
@@ -30,5 +34,30 @@ export class AppComponent implements OnInit {
       console.log(res);
     });
   }
+
+  currentWeekReleaseFromService(){
+    let thisWeek = this._httpService.currentWeekReleases()
+    thisWeek.subscribe(res => {
+      console.log(res);
+    })
+  }
+
+  alreadyOutFromService(){
+    let current = this._httpService.popularReleased();
+    current.subscribe(res => {
+      console.log(res);
+      console.log(res["results"]);
+      for(let x in res["results"]){
+        this.movieList.push(res["results"][x]["title"])
+      }
+      console.log(this.movieList)
+      // let movies = res['results'];
+      // movies.sort(function (a, b) {
+      //   return b.vote_average - a.vote_average
+      // })
+      // console.log(movies);
+    })
+  }
+
 
 }
