e79217ebd90f9998b829a909df5ef757d4082ac4
Matt McCarthy <mattmc318@gmail.com>
2018-02-28 17:32:32 -0600

    Fixed some minor errors.

--- client/src/app/navigation/navigation.component.ts
+++ client/src/app/navigation/navigation.component.ts
@@ -92,8 +92,7 @@ export class NavigationComponent implements OnInit {
     if (type == "name") {
       type = "person";
     } else if (type == "title") {
-      type = "movie",
-        type = "tv"
+      type = "tv";
     } else {
       type = "multi";
     }
